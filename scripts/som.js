d54133b915c63aed966fcaca14c019cdadefb491
Marcos Paulo <marcos.paullo32@gmail.com>
2021-12-13 20:53:01 -0300

    comentários

--- scripts/som.js
+++ scripts/som.js
@@ -1,16 +1,19 @@
-var audio = document.getElementById('audio')
+/* Função utilizada para tocar música durante o jogo, caso o usuário
+opte por escutar */
+
+var audio = document.getElementById('audio') 
 var som = document.getElementById('som')
 var cont = 0
 
-function tocar() {
-    if (cont == 0) {
+function tocar() {   //Executada quando o botão é clicado e faz com toque a música
+    if (cont == 0) { // e também mude a imagem do botão
         cont = 1
         audio.play()
         som.src = '../images/sound.png'
 
         
-    } else {
-        cont = 0
+    } else {       // condição usada para parar o som, e também iniciar o jogo
+        cont = 0  // por padrão no mudo
         audio.pause()
         som.src = '../images/mute.png'
     }
