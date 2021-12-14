ac2f756c2f24e38b101cc1c0291146c000228a9a
oliveiralecca <oliveiraslc@yahoo.com.br>
2021-12-14 15:31:52 -0300

    feat(random): comments on random page code

--- scripts/randomPage.js
+++ scripts/randomPage.js
@@ -1,11 +1,14 @@
+// Função para gerar números inteiros aleatoriamente
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// Chamada à função para gerar números inteiros entre 0 e 4, que corresponde ao total de temas disponíveis no jogo
 let randomNum = getRandomInt(0, 4);
 
+// Função que seleciona a página do tema de acordo com o número gerado acima
 function setPage() {
   switch (randomNum) {
     case 0:
